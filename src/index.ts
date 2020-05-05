b0c78d0688c107ae5cf6b870c6ff93bdbf1248df
Abhilash Kishore <abhilash1in@gmail.com>
2020-05-04 22:14:07 -0700

    Fix lint errors

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,9 @@ import { Inputs } from './constants'
 import { flattenJSONObject, isJSONObjectString, filterBy } from './utils'
 
 // secretNames input string is a new line separated list of secret names. Take distinct secret names.
-let inputSecretNames: string[] = [...new Set(core.getInput(Inputs.SECRET_NAMES).split("\n").filter(x => x !== ""))]
+const inputSecretNames: string[] = [...new Set(core.getInput(Inputs.SECRET_NAMES).split("\n").filter(x => x !== ""))]
 // Check if any secret name contains a wildcard '*'
-let hasWildcard: boolean = inputSecretNames.some(secretName => secretName.includes('*'))
+const hasWildcard: boolean = inputSecretNames.some(secretName => secretName.includes('*'))
 const shouldParseJSON = (core.getInput(Inputs.PARSE_JSON).trim().toLowerCase() === 'true')
 const AWSConfig = {
   accessKeyId: core.getInput(Inputs.AWS_ACCESS_KEY_ID),
@@ -14,14 +14,14 @@ const AWSConfig = {
   region: core.getInput(Inputs.AWS_REGION),
 }
 
-const getSecretsManagerClient = (config) => new SecretsManager(config)
-const getSecretValue = (secretsManagerClient, secretName) => secretsManagerClient.getSecretValue({ SecretId: secretName }).promise()
-const listSecretsPaginated = (secretsManagerClient, nextToken) => secretsManagerClient.listSecrets({ NextToken: nextToken }).promise()
+const getSecretsManagerClient = (config): SecretsManager => new SecretsManager(config)
+const getSecretValue = (secretsManagerClient, secretName): Promise<object> => secretsManagerClient.getSecretValue({ SecretId: secretName }).promise()
+const listSecretsPaginated = (secretsManagerClient, nextToken): Promise<object> => secretsManagerClient.listSecrets({ NextToken: nextToken }).promise()
 
 const listSecrets = (secretsManagerClient: SecretsManager): Promise<Array<string>> => {
   return new Promise<Array<string>>((resolve, reject) => {
     let nextToken: string = null
-    let allSecretNames: string[] = []
+    const allSecretNames: string[] = []
     do {
       listSecretsPaginated(secretsManagerClient, nextToken)
         .then(res => {
@@ -48,7 +48,7 @@ const listSecrets = (secretsManagerClient: SecretsManager): Promise<Array<string
 }
 
 const getSecretValueMap = (secretsManagerClient: SecretsManager,
-  secretName: string, shouldParseJSON: boolean = false): Promise<object> => {
+  secretName: string, shouldParseJSON = false): Promise<object> => {
   return new Promise<object>((resolve, reject) => {
     getSecretValue(secretsManagerClient, secretName)
       .then(data => {
@@ -56,9 +56,9 @@ const getSecretValueMap = (secretsManagerClient: SecretsManager,
         // Decrypts secret using the associated KMS CMK.
         // Depending on whether the secret is a string or binary, one of these fields will be populated.
         if ('SecretString' in data) {
-          secretValue = data.SecretString
+          secretValue = data['SecretString']
         } else {
-          const buff = Buffer.from(data.SecretBinary)
+          const buff = Buffer.from(data['SecretBinary'])
           secretValue = buff.toString('ascii')
         }
         let secretValueMap = {}
@@ -112,7 +112,7 @@ const getSecretNamesToFetch = (secretsManagerClient: SecretsManager, inputSecret
   return new Promise<Array<string>>((resolve, reject) => {
     // list secerts, filter against wildcards and fetch filtered secrets
     // else // fetch specified secrets directly
-    let secretNames: string[] = []
+    const secretNames: string[] = []
     listSecrets(secretsManagerClient)
       .then(secrets => {
         inputSecretNames.forEach(inputSecretName => {
