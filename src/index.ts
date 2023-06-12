9e7c44f8b2aa4aa6a4b87b567e0c7c6e1d5dc0d4
julillosamaral <julio.sarachaga@lagarsoft.com>
2023-06-12 18:45:12 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,7 @@ const shouldParseJSON = core.getBooleanInput(Inputs.PARSE_JSON)
 const AWSConfig = {}
 
 const secretsManagerClient = getSecretsManagerClient(AWSConfig)
+core.debug('Julio esta aca')
 
 if (hasWildcard) {
   core.debug('Found wildcard secret names')
